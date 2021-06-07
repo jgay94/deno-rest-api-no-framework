14a65b46ba05c32dfbf80ddbde3cb525687a90d6
Joseph Gay <49163512+callthecapital@users.noreply.github.com>
2021-06-07 13:27:58 -0400

    add routes and handler functions
    RUN: deno run --allow-net src/app.ts
    TEST: curl -v localhost:8080/about

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as http from "https://deno.land/std/http/server.ts";
+import { ServerRequest } from "https://deno.land/std/http/server.ts";
 
 const PORT = 8080
 const HOSTNAME = "0.0.0.0"
@@ -7,10 +8,31 @@ const server = http.serve({ port: PORT, hostname: HOSTNAME })
 
 console.log(`Server now running at http://${HOSTNAME}:${PORT}`)
 
-for await (const req of server) {
-    const params = new URLSearchParams(req.url.substr(1))
-    const name = params.get("name")
+function handleContact(req:ServerRequest) {
+    req.respond({ body: "This is the contact page", status: 200 })
+}
 
-    req.respond({ body: `Hello ${name ?? "world"}`, status: 200 })
+function handleAbout(req:ServerRequest) {
+    req.respond({ body: "This is the about page", status: 200 })
+}
+
+function handleIndex(req:ServerRequest) {
+    req.respond({ body: "This is the home page", status: 200 })
+}
+
+function handleNotFound(req:ServerRequest) {
+    req.respond({ body: "Page not found", status: 404 })
+}
+
+for await (const req of server) {
+    if (req.url === "/contact" && req.method === "GET")
+        handleContact(req)
+    else if (req.url === "/about" && req.method === "GET")
+        handleAbout(req)
+    else if (req.url === "/" && req.method === "GET")
+        handleIndex(req)
+    else
+        handleNotFound(req)
+    
     console.log(`${req.method} ${req.url}`)
 }
