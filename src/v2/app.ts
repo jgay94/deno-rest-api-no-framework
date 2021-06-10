54721dd9901dbe3e3dad0db67a2f4c8249f7a9b0
Joseph Gay <49163512+callthecapital@users.noreply.github.com>
2021-06-09 20:39:29 -0400

    parse query string

--- src/v2/app.ts
+++ src/v2/app.ts
@@ -3,16 +3,27 @@ import { listenAndServe } from "https://deno.land/std/http/server.ts";
 listenAndServe({ port: 8080 }, (req) => {
   /**
    * 1. parse pathname
-   * 2. parse http method
-   * 3. parse query string, if any
-   * 4. parse body
    */
   const parsedUrl = req.url.replace(/\/+$/g, "").split("?");
   const pathname = parsedUrl[0] || "/";
-  const method = req.method.toLowerCase();
-
   console.log(`pathname: ${pathname}`);
+
+  /**
+   * 2. parse http method
+   */
+  const method = req.method.toLowerCase();
   console.log(`method: ${method}`);
+
+  /**
+   * 3. parse query string
+   */
+  const params = new URLSearchParams(parsedUrl[1]);
+  console.log(`page: ${params.get("page")}`);
+  console.log(`limit: ${params.get("limit")}`);
+
+  /**
+   * 4. parse body
+   */
 
   req.respond({
     body: "Hello from the server",
